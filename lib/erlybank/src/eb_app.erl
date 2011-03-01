24116e5454ee300143332a82b4266fb95eb5648e
Ori Bar <ori.bar@nivertech.com>
2011-03-01 19:18:18 +0200

    better names

--- lib/erlybank/src/eb_app.erl
+++ lib/erlybank/src/eb_app.erl
@@ -1,7 +1,7 @@
--module(erlybank_app).
+-module(eb_app).
 -beahviour(application).
 -export([start/2,stop/1]).
 start(_Type, _Args) ->
-	eb_server_sup:start_link().
+	eb_sup:start_link().
 stop(_State) ->
 	ok.
