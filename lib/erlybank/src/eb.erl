b4d0be7373e2a516bd911a59742d4d16c8df3e6b
Ori Bar <ori.bar@nivertech.com>
2011-03-01 21:14:31 +0200

    fixed eb indentation

--- lib/erlybank/src/eb.erl
+++ lib/erlybank/src/eb.erl
@@ -1,11 +1,15 @@
 -module(eb).
 
--export([start/0,stop/0,create_account/1,deposit/2]).
+-export([start/0, stop/0, create_account/1, deposit/2]).
 
-start() -> application:start(erlybank).
-stop() -> application:stop(erlybank).
-create_account(X) -> eb_server:create_account(X).
-deposit(X,Y) -> eb_server:deposit(X,Y).
+start() -> 
+	application:start(erlybank).
+stop() -> 
+	application:stop(erlybank).
+create_account(X) -> 
+	eb_server:create_account(X).
+deposit(X,Y) -> 
+	eb_server:deposit(X,Y).
 
 
 
