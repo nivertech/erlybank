b2dc9c8fb20c35736e32f1561806c5cc2eb7d270
Ori Bar <ori.bar@nivertech.com>
2011-03-01 23:03:02 +0200

    Refactoring

--- lib/mochieb/src/mochieb_web.erl
+++ lib/mochieb/src/mochieb_web.erl
@@ -20,51 +20,50 @@ start(Options) ->
 stop() ->
     mochiweb_http:stop(?MODULE).
 
+req_handle('GET', ["balance", Name], Req) ->
+    case eb:balance(Name) of
+        {ok,X} ->
+            Req:respond({200, [], 
+                         list_to_binary(integer_to_list(X))});
+        _ ->
+            Req:respond({500, [], []})
+    end;
+req_handle('GET', _, Req) ->
+    Req:not_found();
+req_handle('PUT', ["account", Name], Req) ->
+    case eb:create_account(Name) of
+        ok -> Req:respond({204, [], []});
+        _  -> Req:respond({500, [], []})
+    end;
+req_handle('PUT', _, Req) ->
+    Req:not_found();
+req_handle('POST', ["deposit", Name, Amount], Req) ->
+    case eb:deposit(Name, list_to_integer(Amount)) of
+        {ok,_} -> Req:respond({204, [], []});
+        _      -> Req:respond({500, [], []})
+    end;
+req_handle('POST', ["withdraw", Name, Amount], Req) ->
+    case eb:withdraw(Name, list_to_integer(Amount)) of
+        {ok,_} -> Req:respond({204, [], []});
+        _      -> Req:respond({500, [], []})
+    end;
+req_handle('POST', _, Req) ->
+    Req:not_found();
+req_handle('DELETE', ["account", Name], Req) ->
+    case eb:delete_account(Name) of
+        ok -> Req:respond({204, [], []});
+        _  -> Req:respond({500, [], []})
+    end;
+req_handle('DELETE', _, Req) ->
+    Req:not_found();
+req_handle(_,_,Req) ->
+    Req:respond({501, [], []}).
+
+
 loop(Req, DocRoot) ->
     "/" ++ Path = Req:get(path),
-	Tokens = string:tokens(Path, "/"),
-    case Req:get(method) of
-        Method when Method =:= 'GET'; Method =:= 'HEAD' ->
-            case Tokens of
-				["balance", Name] ->
-					case eb:balance(Name) of
-						{ok,X} ->
-							Req:respond({200, [], 
-										 list_to_binary(integer_to_list(X))});
-						_ ->
-							Req:respond({500, [], []})
-					end;
-                _ ->
-                    Req:not_found()
-            end;
-        'POST' ->
-			case Tokens of
-				["create_account", Name] ->
-					case eb:create_account(Name) of
-						ok -> Req:respond({204, [], []});
-						_  -> Req:respond({500, [], []})
-					end;
-				["deposit", Name, Amount] ->
-					case eb:deposit(Name, list_to_integer(Amount)) of
-						{ok,_} -> Req:respond({204, [], []});
-						_      -> Req:respond({500, [], []})
-					end;
-				["withdraw", Name, Amount] ->
-					case eb:withdraw(Name, list_to_integer(Amount)) of
-						{ok,_} -> Req:respond({204, [], []});
-						_      -> Req:respond({500, [], []})
-					end;
-				["delete_account", Name] ->
-					case eb:delete_account(Name) of
-						ok -> Req:respond({204, [], []});
-						_  -> Req:respond({500, [], []})
-					end;
-				_ ->
-                    Req:not_found()
-            end;
-        _ ->
-            Req:respond({501, [], []})
-    end.
+    Tokens = string:tokens(Path, "/"),
+    req_handle(Req:get(method), Tokens, Req).
 
 %% Internal API
 
