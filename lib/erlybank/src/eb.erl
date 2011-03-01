12c6c4fa4d2fbd830564ddca85d3495eda60f61e
Ori Bar <ori.bar@nivertech.com>
2011-03-01 19:30:08 +0200

    Added eb interface

---
+++ lib/erlybank/src/eb.erl
@@ -0,0 +1,11 @@
+-module(eb).
+
+-export([start/0,stop/0,create_account/1,deposit/2]).
+
+start() -> application:start(erlybank).
+stop() -> application:stop(erlybank).
+create_account(X) -> eb_server:create_account(X).
+deposit(X,Y) -> eb_server:deposit(X,Y).
+
+
+
