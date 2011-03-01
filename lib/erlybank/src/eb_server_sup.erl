0fb46f3307e455560bdcbcf7f784a123c05acbc4
Ori Bar <ori.bar@nivertech.com>
2011-03-01 17:55:34 +0200

    Added supervisor

---
+++ lib/erlybank/src/eb_server_sup.erl
@@ -0,0 +1,14 @@
+-module(eb_server_sup).
+-behaviour(supervisor).
+
+-export([start_link/0,
+		 init/1]).
+
+start_link() ->
+	supervisor:start_link(eb_server_sup, []).
+
+init(_Args) ->
+	{ok, {{one_for_one,100,60},
+		  [{eb_server, {eb_server, start_link, []},
+			permanent, brutal_kill, worker, [eb_server]}]}}.
+
