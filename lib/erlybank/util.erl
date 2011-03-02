82fb6b1d9d483a8f579434a5ef89878e181517e1
Ori Bar <ori.bar@nivertech.com>
2011-03-02 04:09:42 +0200

    integer -> float

---
+++ lib/erlybank/util.erl
@@ -0,0 +1,14 @@
+-module(util).
+-export([list_to_float/1]).
+
+list_to_float(S) ->
+	case string:str(S, ".") of
+		0 ->
+			list_to_integer(S) * 1.0;
+		_ ->
+			erlang:list_to_float(S)
+	end.
+
+float_to_list(X) ->
+	float_to_list(X * 1.0).
+
