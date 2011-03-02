77978216fa02894bc1f76bd66d4b6f1577548a2c
Ori Bar <ori.bar@nivertech.com>
2011-03-02 04:27:56 +0200

    untabify

--- lib/erlybank/src/eb_sup.erl
+++ lib/erlybank/src/eb_sup.erl
@@ -2,13 +2,13 @@
 -behaviour(supervisor).
 
 -export([start_link/0,
-		 init/1]).
+         init/1]).
 
 start_link() ->
-	supervisor:start_link(eb_sup, []).
+    supervisor:start_link(eb_sup, []).
 
 init(_Args) ->
-	{ok, {{one_for_one,100,60},
-		  [{eb_server, {eb_server, start_link, []},
-			permanent, brutal_kill, worker, [eb_server]}]}}.
+    {ok, {{one_for_one,100,60},
+          [{eb_server, {eb_server, start_link, []},
+            permanent, brutal_kill, worker, [eb_server]}]}}.
 
