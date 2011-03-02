e7850bd0766d79a82492deaefa66f4e92335283d
Ori Bar <ori.bar@nivertech.com>
2011-03-02 03:36:38 +0200

    Fix .sh

--- bin/eb_server_web.sh
+++ bin/eb_server_web.sh
@@ -1,6 +1,6 @@
 ERL_LIBS=../lib:$ERL_LIBS
 export ERL_LIBS
 
-erl -sname node_eb -boot start_sasl -eval '[application:start(A)||A<-[erlybank,crypto,inets,mochieb]]'
+erl -sname eb_node -boot start_sasl -eval '[application:start(A)||A<-[erlybank,crypto,inets,mochieb]]'
 
 
