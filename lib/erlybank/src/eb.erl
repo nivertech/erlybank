cccf74c6171961d3037ef389b72475a9a568d6da
Ori Bar <ori.bar@nivertech.com>
2011-03-02 04:27:17 +0200

    untabify

--- lib/erlybank/src/eb.erl
+++ lib/erlybank/src/eb.erl
@@ -3,19 +3,19 @@
 -export([start/0, stop/0, create_account/1, delete_account/1, deposit/2, withdraw/2, balance/1]).
 
 start() -> 
-	application:start(erlybank).
+    application:start(erlybank).
 stop() -> 
-	application:stop(erlybank).
+    application:stop(erlybank).
 create_account(X) -> 
-	eb_server:create_account(X).
+    eb_server:create_account(X).
 delete_account(X) ->
-	eb_server:delete_account(X).
+    eb_server:delete_account(X).
 deposit(X,Y) -> 
-	eb_server:deposit(X,Y).
+    eb_server:deposit(X,Y).
 withdraw(X,Y) ->
-	eb_server:withdraw(X,Y).
+    eb_server:withdraw(X,Y).
 balance(X) ->
-	eb_server:balance(X).
+    eb_server:balance(X).
 
 
 
